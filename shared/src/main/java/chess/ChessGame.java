6d2e938c741649ea3b087e0cd931e1712d0a4778
Zac Conlin <zac.conlin@gmail.com>
2024-04-24 10:15:32 -0600

    Implemented setBoard() and getBoard()

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,6 +10,8 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    ChessBoard gameBoard;
+
     public ChessGame() {
 
     }
@@ -96,7 +98,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        gameBoard = board;
     }
 
     /**
@@ -105,6 +107,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return gameBoard;
     }
 }
