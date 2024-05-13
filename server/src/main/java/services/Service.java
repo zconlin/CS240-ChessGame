01956fb6af2d2eaab7b2c440d812e78c02cdf941
Zac Conlin <zac.conlin@gmail.com>
2024-05-13 12:28:52 -0600

    Added Service.java with getters and setters for each DAO

---
+++ server/src/main/java/services/Service.java
@@ -0,0 +1,48 @@
+package services;
+
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import dataAccess.UserDAO;
+
+public class Service {
+
+    protected AuthDAO authDAO;
+    protected GameDAO gameDAO;
+    protected UserDAO userDAO;
+
+    public Service() {
+        authDAO = new AuthDAO();
+        gameDAO = new GameDAO();
+        userDAO = new UserDAO();
+    }
+
+    public Service(AuthDAO authDAO, GameDAO gameDAO, UserDAO userDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+        this.userDAO = userDAO;
+    }
+
+    public AuthDAO getAuthDAO() {
+        return authDAO;
+    }
+
+    public void setAuthDAO(AuthDAO authDAO) {
+        this.authDAO = authDAO;
+    }
+
+    public GameDAO getGameDAO() {
+        return gameDAO;
+    }
+
+    public void setGameDAO(GameDAO gameDAO) {
+        this.gameDAO = gameDAO;
+    }
+
+    public UserDAO getUserDAO() {
+        return userDAO;
+    }
+
+    public void setUserDAO(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+}
