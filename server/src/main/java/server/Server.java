8dc019290493bd511437a3e4d6985cbdca9854f2
Zac Conlin <zac.conlin@gmail.com>
2024-05-16 09:21:48 -0600

    Created empty objects for the rest of the routes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -19,7 +19,37 @@ public class Server {
     }
 
     private void defineRoutes(){
+        Spark.post("/user", this::registerUser);
+        Spark.post("/session", this::loginUser);
+        Spark.delete("/session", this::logoutUser);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::makeGame);
+        Spark.put("/game", this::joinGame);
         Spark.delete("/db", this::clearDatabase);
+    }
+
+    private Object registerUser(Request req, Response res) {
+        return "";
+    }
+
+    private Object loginUser(Request req, Response res) {
+        return "";
+    }
+
+    private Object logoutUser(Request req, Response res) {
+        return "";
+    }
+
+    private Object listGames(Request req, Response res) {
+        return "";
+    }
+
+    private Object makeGame(Request req, Response res) {
+        return "";
+    }
+
+    private Object joinGame(Request req, Response res) {
+        return "";
     }
 
     private Object clearDatabase(Request req, Response res) {
