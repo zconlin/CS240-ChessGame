873bf0233bc335704b1dc8cf8e292c7c77002dfe
Zac Conlin <zac.conlin@gmail.com>
2024-05-20 09:49:47 -0600

    Created AuthDAO

--- server/src/main/java/dataAccess/AuthDAO.java
+++
@@ -1,9 +0,0 @@
-package dataAccess;
-
-public class AuthDAO {
-
-    public AuthDAO() {
-        super();
-    }
-
-}
---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,28 @@
+package dataaccess;
+
+import model.AuthToken;
+import dataaccess.DataAccessException;
+
+import java.util.HashSet;
+
+public class AuthDAO extends DataAccess {
+
+    private HashSet<AuthToken> authTokens = new HashSet<>();
+
+    public AuthDAO() {
+        super();
+    }
+
+    public void addAuthToken(AuthToken authToken) throws DataAccessException {
+    }
+
+    public boolean checkAuthToken(AuthToken authToken) throws DataAccessException {
+        return false;
+    }
+
+    public void clear(){
+    }
+
+    public void deleteAuthToken(AuthToken authToken) throws DataAccessException {
+    }
+}
