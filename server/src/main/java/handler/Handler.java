4d9687b8409f6269255c746896c1bf71e0a51f8c
Zac Conlin <zac.conlin@gmail.com>
2024-05-20 10:30:03 -0600

    Implemented base Handler class

---
+++ server/src/main/java/handler/Handler.java
@@ -0,0 +1,14 @@
+package handler;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public abstract class Handler implements Route {
+    public Handler() {
+    }
+
+    public abstract Object handle(Request var1, Response var2);
+
+    public abstract requestclasses.Request getRequestClass(Request var1);
+}
