a9c0ba971571f679b5cd3680a645fcc7f2c9b5db
Zac Conlin <zac.conlin@gmail.com>
2024-05-20 09:53:42 -0600

    Created shared model AuthToken

---
+++ shared/src/main/java/model/AuthToken.java
@@ -0,0 +1,33 @@
+package model;
+
+public class AuthToken {
+
+    private String authToken;
+    private String username;
+
+    public AuthToken() {
+        this.authToken = null;
+        this.username = null;
+    }
+
+    public AuthToken(String authToken, String username) {
+        this.authToken = authToken;
+        this.username = username;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
