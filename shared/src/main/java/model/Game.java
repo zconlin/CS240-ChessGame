65e0237e4c97b9c7bdf9a7e18687923ad8596c53
Zac Conlin <zac.conlin@gmail.com>
2024-05-20 09:53:56 -0600

    Created shared model Game

---
+++ shared/src/main/java/model/Game.java
@@ -0,0 +1,68 @@
+package model;
+
+import chess.ChessGame;
+
+public class Game {
+
+    private String gameID;
+    private String whiteUsername;
+    private String blackUsername;
+    private String gameName;
+    private ChessGame game;
+
+    public Game() {
+        this.gameID = null;
+        this.whiteUsername = null;
+        this.blackUsername = null;
+        this.gameName = null;
+        this.game = null;
+    }
+
+    public Game(String gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = game;
+    }
+
+    public String getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(String gameID) {
+        this.gameID = gameID;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
+    public void setWhiteUsername(String whiteUsername) {
+        this.whiteUsername = whiteUsername;
+    }
+
+    public String getBlackUsername() {
+        return blackUsername;
+    }
+
+    public void setBlackUsername(String blackUsername) {
+        this.blackUsername = blackUsername;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    public void setGame(ChessGame game) {
+        this.game = game;
+    }
+}
