552ef8b615caa2032b8ef74b61972444065eab02
Zac Conlin <zac.conlin@gmail.com>
2024-05-28 14:19:01 -0600

    Removed one more unused method

--- shared/src/main/java/model/User.java
+++ shared/src/main/java/model/User.java
@@ -2,7 +2,4 @@ package model;
 
 public record User(String username, String password, String email) {
 
-    public User setUsername(String username) {
-        return new User(username, password, email);
-    }
 }
