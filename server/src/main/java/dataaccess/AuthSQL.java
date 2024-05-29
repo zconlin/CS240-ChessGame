b0586c76ed49f42f30070fc74730971068e02363
zconlin <zac.conlin@gmail.com>
2024-05-29 13:35:00 -0600

    Implemented AuthSQL sql functionality
    Took 6 minutes

--- server/src/main/java/dataaccess/AuthSQL.java
+++ server/src/main/java/dataaccess/AuthSQL.java
@@ -1,8 +1,9 @@
 package dataaccess;
 
 import model.AuthToken;
+import server.ServerException;
 
-import java.util.HashMap;
+import java.sql.SQLException;
 
 public class AuthSQL extends DataAccess {
 
@@ -10,29 +11,107 @@ public class AuthSQL extends DataAccess {
         super();
     }
 
-    public void addAuthToken(AuthToken authToken) throws DataAccessException {
+    public void addAuthToken(AuthToken authToken) throws DataAccessException, ServerException {
+        try (var conn = DatabaseManager.getConnection()) {
+
+            if (authTokenExists(String.valueOf(authToken))) {
+                throw new DataAccessException("Error: Authentication token already exists");
+            }
+
+            var sql = "INSERT INTO chess.auth (authToken, username) VALUES (?, ?);";
+            try (var stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, authToken.getAuthToken());
+                stmt.setString(2, authToken.getUsername());
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                throw new DataAccessException("authError: " + e.getMessage());
+            }
+        } catch (Exception e) {
+            throw new ServerException("Unable to read data", 500);
+        }
     }
 
-    public boolean checkAuthToken(String authToken) throws DataAccessException {
-
+    public boolean checkAuthToken(String authToken) throws DataAccessException, ServerException {
+        if (!authTokenExists(authToken)) {
+            throw new DataAccessException("Error: Authentication token does not exist");
+        }
+        return true;
     }
 
-    public void clear(){
+    public void clear() throws DataAccessException, ServerException {
+        try (var conn = DatabaseManager.getConnection()) {
+
+            var sql = "DELETE FROM chess.auth;";
+            try (var stmt = conn.prepareStatement(sql)) {
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                throw new DataAccessException(e.getMessage());
+            }
+        } catch (Exception e) {
+            throw new ServerException("Unable to read data", 500);
+        }
     }
 
-    public void deleteAuthToken(String authToken) throws DataAccessException {
+    public void deleteAuthToken(String authToken) throws DataAccessException, ServerException {
+        try (var conn = DatabaseManager.getConnection()) {
 
+            if (!authTokenExists(authToken)) {
+                throw new DataAccessException("Error: Authentication token does not exist");
+            }
+
+            var sql = "DELETE FROM chess.auth WHERE authToken = ?;";
+            try (var stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, authToken);
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                throw new DataAccessException("authError: " + e.getMessage());
+            }
+        } catch (Exception e) {
+            throw new ServerException("Unable to read data", 500);
+        }
     }
 
     public AuthToken getAuthToken(String authToken) throws DataAccessException {
 
+        return null;
     }
 
-    public String getUsername(String authToken) {
+    public String getUsername(String authToken) throws DataAccessException, ServerException {
+        try (var conn = DatabaseManager.getConnection()) {
 
+            if (!authTokenExists(authToken)) {
+                throw new DataAccessException("Error: Authentication token does not exist");
+            }
+
+            var sql = "SELECT username FROM chess.auth WHERE authToken = ?;";
+            try (var stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, authToken);
+                var rs = stmt.executeQuery();
+                if (rs.next()) {
+                    return rs.getString("username");
+                }
+            } catch (SQLException e) {
+                throw new DataAccessException("authError: " + e.getMessage());
+            }
+            return null;
+        } catch (Exception e) {
+            throw new ServerException("Unable to read data", 500);
+        }
     }
 
-    public boolean authTokenExists(String authToken) throws DataAccessException {
+    public boolean authTokenExists(String authToken) throws DataAccessException, ServerException {
+        try (var conn = DatabaseManager.getConnection()) {
 
+            var sql = "SELECT * FROM chess.auth WHERE authToken = ?;";
+            try (var stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, authToken);
+                var rs = stmt.executeQuery();
+                return rs.next();
+            } catch (SQLException e) {
+                throw new DataAccessException("authError: " + e.getMessage());
+            }
+        } catch (Exception e) {
+            throw new ServerException("Unable to read data", 500);
+        }
     }
 }
