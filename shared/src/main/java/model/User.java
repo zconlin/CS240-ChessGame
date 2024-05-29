18a5ed4ba517a81495660b7567655734fd0b641b
zconlin <zac.conlin@gmail.com>
2024-05-29 13:15:10 -0600

    Added missing getters to User model
    Took 2 minutes

--- shared/src/main/java/model/User.java
+++ shared/src/main/java/model/User.java
@@ -2,4 +2,15 @@ package model;
 
 public record User(String username, String password, String email) {
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
