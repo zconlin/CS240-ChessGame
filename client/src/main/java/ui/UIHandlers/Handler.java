189396c7ebc9b35433358c76b6261bb627cafd2a
zconlin <zac.conlin@gmail.com>
2024-06-07 22:36:45 -0400

    Added client Handler class
    Took 53 seconds

---
+++ client/src/main/java/ui/UIHandlers/Handler.java
@@ -0,0 +1,14 @@
+package ui.UIHandlers;
+
+import serverfacade.ServerFacade;
+import ui.Printer;
+
+public class Handler {
+    protected final Printer p = new Printer();
+
+    protected final ServerFacade serverFacade;
+
+    public Handler(ServerFacade serverFacade) {
+        this.serverFacade = serverFacade;
+    }
+}
